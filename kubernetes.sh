1cfd094f67c540c940b3bb056432b5f4d40d4964
reddi-devops <reddi.devops@gmail.com>
2020-09-06 20:18:25 +0530

    updated firewall

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,12 +1,14 @@
 setenforce 0
 sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
-firewall-cmd --permanent --add-port=6443/tcp
-firewall-cmd --permanent --add-port=2379-2380/tcp
-firewall-cmd --permanent --add-port=10250/tcp
-firewall-cmd --permanent --add-port=10251/tcp
-firewall-cmd --permanent --add-port=10252/tcp
-firewall-cmd --permanent --add-port=10255/tcp
-firewall-cmd –reload
+systemctl stop firewalld
+systemct disable firewalld
+#firewall-cmd --permanent --add-port=6443/tcp
+#firewall-cmd --permanent --add-port=2379-2380/tcp
+#firewall-cmd --permanent --add-port=10250/tcp
+#firewall-cmd --permanent --add-port=10251/tcp
+#firewall-cmd --permanent --add-port=10252/tcp
+#firewall-cmd --permanent --add-port=10255/tcp
+#firewall-cmd –reload
 modprobe br_netfilter
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 #cp kubernetes.repo /etc/yum.repos.d/
