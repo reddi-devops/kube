e89db65afb617f242764521b8e158498e77c7bff
reddi-devops <reddi.devps@gmail.com>
2020-09-06 14:40:14 +0000

    initial version

---
+++ kubernetes.sh
@@ -0,0 +1,24 @@
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+firewall-cmd --permanent --add-port=6443/tcp
+firewall-cmd --permanent --add-port=2379-2380/tcp
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10251/tcp
+firewall-cmd --permanent --add-port=10252/tcp
+firewall-cmd --permanent --add-port=10255/tcp
+firewall-cmd –reload
+modprobe br_netfilter
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+cp kubernetes.repo /etc/yum.repos.d/
+yum install kubeadm docker -y
+systemctl enable kubelet
+systemctl start kubelet
+systemctl enable docker
+systemctl start docker
+kubeadm init &
+slepp 50
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+export kubever=$(kubectl version | base64 | tr -d '\n')
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
